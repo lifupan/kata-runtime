21dac450a7c9186a3aef5b47d723ac515d679d24
lifupan <fupan.lfp@@antfin.com>
2019-07-09 03:02:22 -0400

    add no_proxy debug
    Signed-off-by: lifupan <fupan.lfp@@antfin.com>

--- virtcontainers/no_proxy.go
+++ virtcontainers/no_proxy.go
@@ -6,7 +6,11 @@
 package virtcontainers
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/sirupsen/logrus"
+	"io"
+	"net"
 )
 
 // This is the no proxy implementation of the proxy interface. This
@@ -28,12 +32,17 @@ func (p *noProxy) start(params proxyParams) (int, string, error) {
 		return -1, "", fmt.Errorf("proxy logger is not set")
 	}
 
-	params.logger.Debug("No proxy started because of no-proxy implementation")
+	params.logger.Debug("---------------------No proxy started because of no-proxy implementation")
+	
+	params.logger.Debugf("========================watchconsole on %s", params.consoleURL)
 
 	if params.agentURL == "" {
 		return -1, "", fmt.Errorf("AgentURL cannot be empty")
 	}
-
+	err := p.watchConsole(buildinProxyConsoleProto, params.consoleURL, params.logger)
+	if err != nil {
+		return -1, "", err
+	}
 	return 0, params.agentURL, nil
 }
 
@@ -44,5 +53,51 @@ func (p *noProxy) stop(pid int) error {
 
 // The noproxy doesn't need to watch the vm console, thus return false always.
 func (p *noProxy) consoleWatched() bool {
-	return false
+       return false
+}
+
+func (p *noProxy) watchConsole(proto, console string, logger *logrus.Entry) (err error) {
+	var (
+		scanner *bufio.Scanner
+		conn    net.Conn
+	)
+
+	switch proto {
+	case consoleProtoUnix:
+		conn, err = net.Dial("unix", console)
+		if err != nil {
+			return err
+		}
+		// TODO: add pty console support for kvmtools
+	case consoleProtoPty:
+		fallthrough
+	default:
+		return fmt.Errorf("unknown console proto %s", proto)
+	}
+
+	logger.Infof("========================watchconsole on %s", console)
+//	p.conn = conn
+
+	go func() {
+		scanner = bufio.NewScanner(conn)
+		for scanner.Scan() {
+			logger.WithFields(logrus.Fields{
+//				"sandbox":   p.sandboxID,
+				"vmconsole": scanner.Text(),
+			}).Debug("reading guest console")
+		}
+
+		if err := scanner.Err(); err != nil {
+			if err == io.EOF {
+				logger.Info("console watcher quits")
+			} else {
+				logger.WithError(err).WithFields(logrus.Fields{
+					"console-protocol": proto,
+					"console-socket":   console,
+				}).Error("Failed to read agent logs")
+			}
+		}
+	}()
+
+	return nil
 }
